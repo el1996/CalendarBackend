323081ba29b54b112e4733597b18917fa0e1630e
Elchanan <elchananm10@gmail.com>
2023-01-27 03:00:48 +0200

    Initial commit

---
+++ src/main/java/calendar/exception/ControllerAdvisor.java
@@ -0,0 +1,29 @@
+package calendar.exception;
+
+import calendar.exception.customException.ValidationErrorException;
+import calendar.responsHandler.ErrorResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Map;
+
+@ControllerAdvice
+public class ControllerAdvisor extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse<String>> handleIllegalArgumentException(IllegalArgumentException ex) {
+        logger.error(ex.getMessage());
+        ErrorResponse<String> errorResponse = new ErrorResponse<>(ex.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(ValidationErrorException.class)
+    public ResponseEntity<ErrorResponse<Map<String, String>>> handleValidationErrorException(ValidationErrorException ex, WebRequest request) {
+        Map<String, String> errors = ex.getErrors();
+        ErrorResponse<Map<String, String>> errorResponse = new ErrorResponse<>(errors);
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+}
---
+++ src/test/java/calendar/service/UserServiceTest.java
@@ -0,0 +1,34 @@
+package calendar.service;
+
+import calendar.entities.User;
+import calendar.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+    @InjectMocks
+    private UserService service;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Test
+    public void testFindById_Success() {
+        int id = 1;
+        User user = new User();
+        when(userRepository.findById(id)).thenReturn(Optional.of(user));
+        User result = service.findById(id);
+        assertEquals(user, result);
+    }
+
+}
